dd5a99483015f4e565db0e1d0aa248adb63cb821
Miguel Galindo <miguelgalindo1214@gmail.com>
2026-05-08 14:57:59 -0400

    Initial backend skeleton

---
+++ src/main/java/com/soccerdev/SoccerDevelopmentApiApplication.java
@@ -0,0 +1,12 @@
+package com.soccerdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerDevelopmentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoccerDevelopmentApiApplication.class, args);
+    }
+}
